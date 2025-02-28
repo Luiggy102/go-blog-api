d11b26da894da7a2067029cc42b3bbe89dd7cb59
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2025-02-28 01:19:39 -0500

    fixed mongo-init.js not working

--- database/mongo-init-scripts/mongo-init.js
+++ database/mongo-init-scripts/mongo-init.js
@@ -1,7 +1,6 @@
-use go_blog;
 db.posts.insertOne(
     {
-        post_content: 'Post Demo',
+        post_content: 'My first Post!',
         created_at: ISODate('2025-01-23T15:45:31.673Z'),
         updated_at: ISODate('2025-01-23T15:45:31.673Z')
     }
